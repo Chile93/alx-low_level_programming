723053757a0ee9ca92e74d934963f96771bb57c5
Chinedu Agwunobi <79590894+Chile93@users.noreply.github.com>
2022-03-10 20:00:41 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /*
- * main - prints the phrase "Programming is like building a multilingual puzzle
+ * main -  prints the phrase "Programming is like building a multilingual puzzle
  *
  * Return: 0 if exited properly, non zero otherwise
  */
@@ -12,8 +12,8 @@ int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-		return (0);
+	return (0);
 
 }

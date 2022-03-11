880b46eee33a517ed0397149083a51b6e5454099
Chinedu Agwunobi <79590894+Chile93@users.noreply.github.com>
2022-03-11 03:05:42 -0800

    4-puts, 5-printf, 6-size

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,23 +1,17 @@
 #include <stdio.h>
 
 
-
 /**
- *main - Entry point,
+ * main - Entry point,
  *
  * Description : rints the phrase with proper grammar,
  * but the outcome is a piece of art
  *
- * Return: 0
+ * Return : Always 0 (Success)
  *
  */
-
 int main(void)
-
 {
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
-
 }

ac218864c30f61114d17e70d7684f259826a29b1
Chinedu Agwunobi <79590894+Chile93@users.noreply.github.com>
2022-03-11 03:52:55 -0800

    puts,printf,size

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,10 @@
  *
  * Description : prints "Programming is like building a multilingual puzzle"
  *
- * Return : Always 0 (Succes)
+ * Return : Always 0 (Success)
  */
 int main(void)
 {
-        	puts("\"Programming is like building a multilingual puzzle");
-        	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

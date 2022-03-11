7f833de7b2819105da82165f745b1b60eeb9e174
Chinedu Agwunobi <79590894+Chile93@users.noreply.github.com>
2022-03-10 20:59:22 -0800

    Print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 
 
 /**
+ *main - Entry point,
  *
- *  main - prints the phrase with proper grammar,
+ * Description : rints the phrase with proper grammar,
+ * but the outcome is a piece of art
  *
- * but the outcome is a piece of art,
- *
- * Return: 0 if exited properly, non-zero otherwise
+ * Return: 0
  *
  */
 

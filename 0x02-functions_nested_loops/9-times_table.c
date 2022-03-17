707971ffcc6e2c3ab69043870028f3dc2204503b
Chinedu Agwunobi <79590894+Chile93@users.noreply.github.com>
2022-03-17 12:09:19 -0700

    prints the 9 times table, starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,11 +8,9 @@
 
 void times_table(void)
 {
-
 	int i, j, k;
 
 	for (i = 0; i < 10; i++)
-
 	{
 
 		for (j = 0; j < 10; j++)
@@ -49,13 +47,8 @@ void times_table(void)
 				_putchar((k % 10) + '0');
 
 			}
-
 			_putchar('\n');
 
-
-
 		}
-
 	}
-
 }
